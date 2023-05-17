2802e598c7a05b1e756394cfd6037ef06639ed8b
xrsantos <xricardosantos@gmail.com>
2023-05-17 18:26:21 -0300

    ,

---
+++ sistemaDB/Repository/ClientSystemRepository.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using sistemaDB.Models;
+
+namespace sistemaDB.Repository
+{
+    public class ClientSystemRepository: IClientSystemRepository
+    {
+
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClientSystem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientSystem GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(ClientSystem ClientSystem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ClientSystem ClientSystem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+   
